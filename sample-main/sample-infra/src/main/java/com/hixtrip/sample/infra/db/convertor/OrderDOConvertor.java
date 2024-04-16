4d1647d42f225f4b70600dde7325b07216dd61d5
BlackWent <2050446216@qq.com>
2024-04-17 00:40:13 +0800

    Revert "功能完成，提交"
    This reverts commit 57acad681305e5071e10091c4c6710638a600d52.

--- sample-main/sample-infra/src/main/java/com/hixtrip/sample/infra/db/convertor/OrderDOConvertor.java
+++ sample-main/sample-infra/src/main/java/com/hixtrip/sample/infra/db/convertor/OrderDOConvertor.java
@@ -1,7 +1,5 @@
 package com.hixtrip.sample.infra.db.convertor;
 
-import com.hixtrip.sample.domain.order.model.Order;
-import com.hixtrip.sample.infra.db.dataobject.OrderDO;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
@@ -12,6 +10,5 @@ import org.mapstruct.factory.Mappers;
 @Mapper
 public interface OrderDOConvertor {
     OrderDOConvertor INSTANCE = Mappers.getMapper(OrderDOConvertor.class);
-    Order doToDomain(OrderDO orderDO);
-    OrderDO domainToDO(Order order);
+
 }
